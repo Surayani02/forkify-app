ba61a522006c5382c1dab7c178d5a5f4c691a78f
Surajit Saha <imsur02@outlook.com>
2022-09-09 16:11:04 +0530

    Small bug fixed in model.js file

--- src/js/model.js
+++ src/js/model.js
@@ -65,8 +65,8 @@ export const loadSearchResults = async function (query) {
 
 export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
-  const start = (page - 1) * state.search.resultsPerPage,
-    end = page * this.state.search.resultsPerPage;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
   return state.search.results.slice(start, end);
 };
 
